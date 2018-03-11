45278546a50c7eb77a0412549e4f18e5acf43dd3
David Tosh <david.tosh@lm-c02vh2ldhv2q.local>
2018-03-12 12:16:24 +1300

    Initial Commit

---
+++ git test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  git test
+//
+//  Created by David Tosh on 12/03/18.
+//  Copyright © 2018 David Tosh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
